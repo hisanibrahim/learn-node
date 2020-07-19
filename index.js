32426a967de50037e8feea370f2fd851526f30cf
Hisan Ibrahim <hisanibrahim@gmail.com>
2020-07-19 12:06:24 +0530

    parameters in GET

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// CRUD Operations
+
+// Create, Read, Update, Delete
+
+// POST, GET, PUT, DELETE
+
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
@@ -12,11 +18,21 @@ app.get("/api/courses", (req, res) => {
   ]);
 });
 
+// Route parameters
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+// Query string parameters
+
+app.get("/api/posts", (req, res) => {
+  // http://localhost:3000/api/posts?name=hisan&age=24
+  res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
-// CRUD Operations
-
-// Create, Read, Update, Delete
-
-// POST, GET, PUT, DELETE
