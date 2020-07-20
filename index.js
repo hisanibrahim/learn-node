014284e4d7fdeac4717831e6bd78d8dc215f677a
Hisan Ibrahim <hisanibrahim@gmail.com>
2020-07-20 14:04:51 +0530

    handle http GET requests

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 const express = require("express");
 const app = express();
 
-// CRUD Operations
-
-// Create, Read, Update, Delete
-
-// POST, GET, PUT, DELETE
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([
-    { id: 1, name: "hello" },
-    { id: 2, name: "world" },
-  ]);
+  res.send(courses);
 });
 
 // Route parameters
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params);
-});
-
-app.get("/api/posts/:year/:month", (req, res) => {
-  res.send(req.params);
-});
-
-// Query string parameters
-
-app.get("/api/posts", (req, res) => {
-  // http://localhost:3000/api/posts?name=hisan&age=24
-  res.send(req.query);
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course) res.status(404).send("Course not found");
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
