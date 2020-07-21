d21a362f455af0f114e9ee2e0069190385864bb6
Hisan Ibrahim <hisanibrahim@gmail.com>
2020-07-21 09:53:07 +0530

    handle http PUT requests. seperate method to validate course use in two places.

--- index.js
+++ index.js
@@ -19,11 +19,7 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  const { value, error } = schema.validate(req.body);
+  const { error } = validateCourse(req.body);
   if (error) {
     res.status(400).send(error.details[0].message);
     return;
@@ -44,6 +40,32 @@ app.get("/api/courses/:id", (req, res) => {
   if (!course) res.status(404).send("Course not found");
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course) res.status(404).send("Course not found");
+
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+
+  res.send(course);
+});
+
+const validateCourse = (course) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+};
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
